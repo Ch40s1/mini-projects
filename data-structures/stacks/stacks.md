6f3c60b2e2c611c92ffac0c7838f9c4a890818e5
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-04-13 23:37:12 -0500

    added push, peek for stack

--- data-structures/stacks/stacks.md
+++ data-structures/stacks/stacks.md
@@ -1,1 +1,20 @@
-# hello
+# Stacks
+
+## what is a stack?
+A Stack is an abstract data type that serves as a collection of elements. It uses LIFO ( last in first out).
+It can store any data.
+
+## What it has?
+
+* stack.push(item) -> places a new item on top of the stack
+* stack.pop() -> removes the top item from the stack and returns it
+* stack.peek() -> returns the top item from the stack without modifying the stack
+* stack.size() -> returns the number of items in the stack
+
+## Notes
+I visualize them as a stack of plates. We grab from the top downward. The time complexity of the push, pop, peek and size methods
+is O(1). The time complexity to remove the last item is O(n) because we would need to remove all the items on top and work
+our way downward.
+
+
+
--- data-structures/stacks/stacks.py
+++ data-structures/stacks/stacks.py
@@ -1,1 +1,33 @@
-print("hello world")
+''' 
+A real-world example of a stack could be the browser history feature in web browsers.
+
+ When you visit a web page, the URL of that page is pushed onto a stack. If you click on a link to navigate to another page, 
+ the URL of the current page is pushed onto the stack again. This process continues for each page you visit.
+
+When you click the "Back" button in the browser, it pops the URL from the stack, effectively 
+taking you back to the previously visited page. If you keep clicking "Back", it continues to pop URLs from the stack, 
+effectively navigating you back through your browsing history.
+'''
+
+
+class Stack:
+    def __init__(self):
+        self.websites = []
+
+    # stack.push(website) -> add a website to teh list
+    def push(self, site):
+        self.websites.append(site)
+    # stack.peek() -> returns the top item from the stack without modifying the stack
+
+    def peek(self):
+        if len(self.websites) != 0:
+            return self.websites[-1]
+        else:
+            print("Stack is empty")
+    # stack.size() -> returns the number of items in the stack
+
+
+stack = Stack()
+stack.push("google.com")
+
+print("Top of the stack", stack.peek())
